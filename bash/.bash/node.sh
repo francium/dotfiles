c1d5cd81e521cae6b644645297c497a0e269465d
francium <francium@francium.cc>
2021-12-06 13:54:16 -0500

    Bash: Update node bash config for OS compatibility

--- bash/.bash/node.sh
+++ bash/.bash/node.sh
@@ -1,11 +1,24 @@
 # fnm
-FNM_PATH=~/.fnm
-if [[ -d $FNM_PATH ]]; then
-    export PATH=$FNM_PATH:$PATH
-    eval "$(fnm env)"
-else
-    alias nvm="echo install fnm from https://github.com/Schniz/fnm"
-    alias fnm="echo install fnm from https://github.com/Schniz/fnm"
-    alias node="echo install fnm from https://github.com/Schniz/fnm"
-    alias nvm="echo install fnm from https://github.com/Schniz/fnm"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    FNM_PATH=~/.fnm
+    if [[ -d $FNM_PATH ]]; then
+        export PATH=$FNM_PATH:$PATH
+        eval "$(fnm env)"
+    else
+        MSG="echo install fnm from https://github.com/Schniz/fnm"
+        alias nvm=$ALIAS
+        alias fnm=$ALIAS
+        alias node=$ALIAS
+        alias npm=$ALIAS
+    fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    if command -v fnm; then
+        eval "$(fnm env)"
+    else
+        MSG="echo brew install fnm"
+        alias nvm=$ALIAS
+        alias fnm=$ALIAS
+        alias node=$ALIAS
+        alias npm=$ALIAS
+    fi
 fi
