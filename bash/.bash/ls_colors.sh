8f5419d58c0a02903815d534795c19b90f136057
francium <francium@francium.cc>
2022-04-14 11:05:32 -0400

    Bash: Update colors

--- bash/.bash/ls_colors.sh
+++ bash/.bash/ls_colors.sh
@@ -1,1 +1,5 @@
-export LS_COLORS=$(vivid generate ~/.config/vivid/themes/molokai-custom)
+if [ -n "$d" ]; then
+        export LS_COLORS=$(vivid generate ~/.config/vivid/themes/molokai-custom-dark)
+else
+        export LS_COLORS=$(vivid generate ~/.config/vivid/themes/molokai-custom-light)
+fi
