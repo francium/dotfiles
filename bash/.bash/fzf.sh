1b5af35d97905888526fe0300441ac91a864cd72
francium <francium@francium.cc>
2023-12-23 23:51:50 -0500

    Git: Disable unused fzf bash functions

--- bash/.bash/fzf.sh
+++ bash/.bash/fzf.sh
@@ -37,11 +37,11 @@ is_in_git_repo() {
   git rev-parse HEAD > /dev/null 2>&1
 }
 
-gf() {
-  is_in_git_repo &&
-    git -c color.status=always status --short |
-    fzf --height 40% -m --ansi --nth 2..,.. | awk '{print $2}'
-}
+# gf() {
+#   is_in_git_repo &&
+#     git -c color.status=always status --short |
+#     fzf --height 40% -m --ansi --nth 2..,.. | awk '{print $2}'
+# }
 
 # Git branch
 gb() {
@@ -52,30 +52,30 @@ gb() {
 }
 
 # Git all branches
-gbr() {
-  is_in_git_repo &&
-    git branch --sort=committerdate -vv --color=always --all | grep -v '/HEAD\s' |
-    fzf --height 40% --ansi --multi --tac | sed 's/^..//' | awk '{print $1}' |
-    sed 's#^remotes/[^/]*/##'
-}
+# gbr() {
+#   is_in_git_repo &&
+#     git branch --sort=committerdate -vv --color=always --all | grep -v '/HEAD\s' |
+#     fzf --height 40% --ansi --multi --tac | sed 's/^..//' | awk '{print $1}' |
+#     sed 's#^remotes/[^/]*/##'
+# }
 
-gt() {
-  is_in_git_repo &&
-    git tag --sort -version:refname |
-    fzf --height 40% --multi
-}
+# gt() {
+#   is_in_git_repo &&
+#     git tag --sort -version:refname |
+#     fzf --height 40% --multi
+# }
 
-gh() {
-  is_in_git_repo &&
-    git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph |
-    fzf --height 40% --ansi --no-sort --reverse --multi | grep -o '[a-f0-9]\{7,\}'
-}
+# gh() {
+#   is_in_git_repo &&
+#     git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph |
+#     fzf --height 40% --ansi --no-sort --reverse --multi | grep -o '[a-f0-9]\{7,\}'
+# }
 
-gr() {
-  is_in_git_repo &&
-    git remote -v | awk '{print $1 " " $2}' | uniq |
-    fzf --height 40% --tac | awk '{print $1}'
-}
+# gr() {
+#   is_in_git_repo &&
+#     git remote -v | awk '{print $1 " " $2}' | uniq |
+#     fzf --height 40% --tac | awk '{print $1}'
+# }
 
 # bind '"\er": redraw-current-line'
 # bind '"\C-g\C-f": "$(gf)\e\C-e\er"'
